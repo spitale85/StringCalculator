671b8dedc2391a8971a1c3521f86fcfcd379dcae
spitale85 <spitale@hotmail.it>
2023-08-30 20:33:24 +0200

    Step 1 completed

---
+++ .src/StringCalculator.Shared/Model/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Xml;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace StringCalculator.Shared.Model
+{
+    public class Calculator
+    {
+        public int Add(string[] numbers)
+        {
+            int result = 0;
+            
+            foreach (var number in numbers)
+            {
+                if (string.IsNullOrEmpty(number)) return 0;
+                result += int.Parse(number);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ .tests/StringCalculator.Tests/Model/CalculatorAdd.cs
@@ -0,0 +1,66 @@
+﻿using StringCalculator.Shared.Model;
+
+namespace StringCalculator.Tests.Model
+{
+    public class CalculatorAdd
+    {
+        private readonly Calculator _calculator;
+
+        public CalculatorAdd()
+        {
+                _calculator = new Calculator();
+        }
+        
+        [Fact]
+        public void ReturnZeroGivenEmptyStrings()
+        {
+            // Arrange
+            //var calculator = new Calculator();
+
+            // Act
+            int result = _calculator.Add(new string[] { "" });
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void ReturnOneGivenOne()
+        {
+            // Arrange
+            //var calculator = new Calculator();
+
+            // Act
+            int result = _calculator.Add(new string[] { "1" });
+
+            // Assert
+            Assert.Equal(1, result);
+        }
+
+        [Fact]
+        public void ReturnTwoGivenTwo()
+        {
+            // Arrange
+            //var calculator = new Calculator();
+
+            // Act
+            int result = _calculator.Add(new string[] {"2"});
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void ReturnNumbersSumGivenAnyNumbers()
+        {
+            // Arrange
+            //var calculator = new Calculator();
+
+            // Act
+            int result = _calculator.Add(new string[] {"1", "2"});
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
